d5f856a3d3aed8c69d0c7b718040090a9734a278
SahilMamedov <sahilim@code.edu.az>
2022-05-22 15:23:14 +0400

    Feat: Custom array

--- Assest/script.js
+++ Assest/script.js
@@ -30,3 +30,46 @@ divide(divideNum){
 var result= new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2)
 
  console.log(result)
+
+ // 2) Custom bir array classi yaradin.
+ // Hemin arrayin find ve push metodu olsun. Taski ishleyib turn in edin.
+
+ class CustomArray{
+
+  constructor(array){
+      this.array=array
+  }
+
+  find(id){
+    let value
+    for (let i = 0; i < this.array.length; i++) {
+      if(this.array[i]==id){
+
+        return value=this.array[i]
+    
+      }
+      
+    }
+    return value
+  }
+
+
+push(value){
+
+    this.array[length]=value
+    this.array[length++]
+    return this.array
+
+}
+
+
+ }
+ var customarr= new CustomArray([])
+ customarr.push("salam")
+ customarr.push("hello")
+ customarr.push(543)
+ customarr.push(221)
+
+ console.log(customarr)
+ console.log(customarr.find(221))
+ 
